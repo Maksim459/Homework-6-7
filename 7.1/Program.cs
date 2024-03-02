c01c93880aad089df21d4dfd8c4dc92542c70a7a
Maksim459 <159301480+Maksim459@users.noreply.github.com>
2024-03-03 00:47:54 +0300

    Add files via upload

---
+++ 7.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ClassLibrary7;
+class Program
+{
+    static void Main()
+    {
+       
+        ClassLibrary7.ArrayOperations arrayOps = new ClassLibrary7.ArrayOperations();
+
+        int[] inputArray = { 1, 2, 3, 4, 5 };
+
+        int[] reversedArray = arrayOps.ReverseArray(inputArray);
+      
+        Console.WriteLine("Исходный массив:");
+        PrintArray(inputArray);
+
+        Console.WriteLine("Инвертированный массив:");
+        PrintArray(reversedArray);
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 7.2/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("введите высоту");
+      double h=Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("введите радиус");
+      double r = Convert.ToDouble(Console.ReadLine());
+        double sful = Cone.Class1.Sfull(h, r);
+        double spov = Cone.Class1.Spov(r);
+        Console.WriteLine("площадь полной поверхности конуса");
+        Console.WriteLine(sful);
+        Console.WriteLine("площадь боковой поверхности конуса");
+        Console.WriteLine(spov);
+    }
+}
