87c07725411c9069b459b56c19a5ed4b855cf86a
Maksim459 <159301480+Maksim459@users.noreply.github.com>
2024-03-06 17:35:16 +0300

    Add files via upload

---
+++ ClassLibrary7/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassLibrary7
+{
+    public class ArrayOperations
+    {
+        
+        public int[] ReverseArray(int[] array)
+        {
+            int length = array.Length;
+            int[] reversedArray = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                reversedArray[i] = array[length - 1 - i];
+            }
+
+            return reversedArray;
+        }
+    }
+}
---
+++ Cone/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Cone
+{
+    public class Class1
+    {
+       
+        
+        public static double Sfull(double h,double r)
+        {
+            double Pi = 3.14;
+            return Pi*r*r+(Pi*r*(Math.Sqrt(r*r+h*h)));
+        }
+        public static double Spov(double r)
+        {
+            double Pi = 3.14;
+            return Pi * r * r ;
+        }
+    }
+   
+
+}
